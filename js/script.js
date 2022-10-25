f02987bfc0a866a1b62c57a2d611c2dc09fb074c
mboufal <mboufal@hawk.iit.edu>
2022-10-24 20:56:45 -0500

    update index

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-function sayThanks(name) {
-  console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
+document.querySelector('#calculate').onclick = function(){
+  var bill = Number(document.getElementById('amount').value);
+  var tip = document.getElementById('rangeValue').value;
+  document.getElementById('tipOutput').innerHTML = `${tip}%`;
+  var tipValue = bill * (tip/100)
+  var finalBill = bill + tipValue
+  console.log(finalBill)
+  var tipAmount = document.querySelector('#tipAmount')
+  var totalBillWithTip = document.querySelector('#totalBillWithTip')
+
+tipAmount.value = tipValue.toFixed(2);
+ totalBillWithTip.value =finalBill.toFixed(2);
+
+ //Show Results
+
+  document.getElementById('results').style.display='block'
 }
-sayThanks('Cole');
